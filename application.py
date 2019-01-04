4dc9b022eb8c01a3938c49477cd22ffaec67d33c
Harald Lønsethagen <haraldlons@gmail.com>
2019-01-04 14:38:55 +0100

    Update application.py

--- application.py
+++ application.py
@@ -115,6 +115,7 @@ welcome = """
     <p>Update: 14:18</p>
     <p>Update: 14:27</p>
     <p>Update: 14:34</p>
+    <p>Update: 14:38</p>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
