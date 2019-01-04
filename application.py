180d43c2ba7e508bbb51b86c98d0876a0fa32157
Harald Lønsethagen <haraldlons@gmail.com>
2019-01-04 14:10:01 +0100

    Update application.py

--- application.py
+++ application.py
@@ -110,7 +110,8 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations: Awesome</h1>
+    <h1>Congratulations: </h1>
+    <p>Update: 14:09</p>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
